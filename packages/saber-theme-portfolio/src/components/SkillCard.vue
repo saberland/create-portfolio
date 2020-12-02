2a8a1c93fe2691939c259b8edd8e16f36671cc9d
sjpratt19 <sjpratt19@gmail.com>
2020-12-01 16:49:12 -0800

    Changed SkillCard text colour to match rest of site

--- packages/saber-theme-portfolio/src/components/SkillCard.vue
+++ packages/saber-theme-portfolio/src/components/SkillCard.vue
@@ -44,7 +44,7 @@ export default {
 }
 
 .skill-description {
-  color: var(--text-color-lighter);
+  color: var(--text-color-light);
   display: block;
   text-overflow: ellipsis;
   word-wrap: break-word;
